df713242ec67238198e6d9a23cfdd6eaccb117d5
katskurka <kati.skurka@gmail.com>
2021-07-07 20:31:28 -0400

    fixed code to get a passing test

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function spotEachRebel(rebels) {
   let spottedRebelCalls = []
+  let i = 0
 
-  if (rebels) {
+  while (i < rebels.length) {
     spottedRebelCalls.push('Look! Rebel scum!')
+    i++
   }
 
   return spottedRebelCalls
@@ -12,10 +14,14 @@ function seeRebelsEscape(rebels) {
   let escapedRebelCalls = []
   let i = 0
 
-  do {
+  while (rebels[i] && rebels[i]['plotArmor']) {
     escapedRebelCalls.push('Oh no! They\'re getting away!')
     i++
-  } while (rebels[i] && rebels[i]['plotArmor'])
+  }
+  // do {
+  // escapedRebelCalls.push('Oh no! They\'re getting away!')
+  // i++
+  // } while (rebels[i] && rebels[i]['plotArmor'])
 
   return escapedRebelCalls
 }
@@ -23,7 +29,7 @@ function seeRebelsEscape(rebels) {
 function shootAtNothing() {
   let shotsAtNothing = []
 
-  for (let i = 1; i < 4; i++) {
+  for (let i = 0; i < 4; i++) {
     shotsAtNothing.push('pew')
   }
 
